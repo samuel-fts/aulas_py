2bc94f417f3a71f32a7b48f33f20f35bdb28be07
Samuel Freitas <samuel.fts.s.8@gmail.com>
2025-02-19 11:29:32 -0300

    AULAS NOVAS

---
+++ aula12.py
@@ -0,0 +1,17 @@
+for c in range (0,6,+1):#de um numero ate o outro 
+    print("oi {}".format(c))#lembre-se do format para implemetar a variavel dentro do texto em string
+print("fim")
+
+n=int(input("digite um numero"))
+
+for n in range(0,n+1):
+    print(n)
+print("FIM")
+
+i = int(input("inicio"))
+f = int(input("fim"))
+p = int(input("passo"))
+
+for c in range(i,f,p):
+    print(c)
+print("FIM")
---
+++ exerc4.py
@@ -0,0 +1,5 @@
+e = 50
+for c in range(0,50,):
+    if c % 2 == 0:
+        print(c, end=" ")
+
